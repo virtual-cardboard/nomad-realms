bc9e0a14c810a6dde613a072bd9fb8b86d501c88
Lunkle <bobbob.donny@gmail.com>
2021-08-27 13:56:22 -0400

    Update NomadRealmsTestClient.java

--- src/main/java/main/NomadRealmsTestClient.java
+++ src/main/java/main/NomadRealmsTestClient.java
@@ -1,9 +1,9 @@
 package main;
 
+import context.BootstrapData;
+import context.BootstrapInput;
+import context.BootstrapLogic;
 import context.GameContext;
-import context.STUNData;
-import context.STUNInput;
-import context.STUNLogic;
 import context.data.GameData;
 import context.input.GameInput;
 import context.logic.GameLogic;
@@ -14,9 +14,9 @@ import engine.GameEngine;
 public class NomadRealmsTestClient {
 
 	public static void main(String[] args) {
-		GameData data = new STUNData();
-		GameInput input = new STUNInput();
-		GameLogic logic = new STUNLogic();
+		GameData data = new BootstrapData();
+		GameInput input = new BootstrapInput();
+		GameLogic logic = new BootstrapLogic();
 		GameVisuals visuals = new DefaultGameVisuals();
 		GameContext context = new GameContext(data, input, logic, visuals);
 		new GameEngine("Nomad Realms Test Client", context)
