4cef52db6ff3801c887f1ea312d8b7fe96437c57
Lunkle <bobbob.donny@gmail.com>
2021-10-24 16:01:26 -0400

    Removed debug statement

--- src/main/java/context/connect/PeerConnectInput.java
+++ src/main/java/context/connect/PeerConnectInput.java
@@ -10,7 +10,6 @@ public class PeerConnectInput extends GameInput {
 	@Override
 	protected void init() {
 		addPacketReceivedFunction(new GameInputEventHandler<>((event) -> {
-			System.out.println("Hi");
 			return fromPacket(event.getModel());
 		}));
 	}
