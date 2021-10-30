5020bc87148de5789da8fbdcddc305f8d218ee3f
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-10-30 12:38:18 -0400

    Create CardGui.java

---
+++ src/main/java/graphics/gui/CardGui.java
@@ -0,0 +1,13 @@
+package graphics.gui;
+
+import common.math.Matrix4f;
+import context.GLContext;
+import context.visuals.gui.Gui;
+
+public class CardGui extends Gui {
+
+	@Override
+	public void render(GLContext glContext, Matrix4f matrix4f, float x, float y, float width, float height) {
+	}
+
+}
