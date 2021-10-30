cbe22e3237ea2d0b2575f357ef62bc2a75574eb1
Lunkle <bobbob.donny@gmail.com>
2021-10-30 12:30:06 -0400

    LocalUser as source for network events

--- src/main/java/context/connect/PeerConnectLogic.java
+++ src/main/java/context/connect/PeerConnectLogic.java
@@ -35,7 +35,7 @@ public class PeerConnectLogic extends GameLogic {
 		while (!eventQueue().isEmpty()) {
 			GameEvent poll = eventQueue().poll();
 			if (poll instanceof PeerConnectRequestEvent || poll instanceof PeerConnectResponseEvent) {
-				PeerConnectResponseEvent connectResponse = new PeerConnectResponseEvent(null);
+				PeerConnectResponseEvent connectResponse = new PeerConnectResponseEvent();
 				context().sendPacket(toPacket(connectResponse, PEER_ADDRESS));
 				System.out.println("Connected with " + PEER_ADDRESS + "!");
 				transitionToGame();
@@ -47,7 +47,7 @@ public class PeerConnectLogic extends GameLogic {
 			if (data.timesTried() >= MAX_RETRIES) {
 				System.out.println("Failed to connect!");
 			} else if (time - data.lastTriedTime() >= TIMEOUT_MILLISECONDS) {
-				PeerConnectRequestEvent connectRequest = new PeerConnectRequestEvent(0, null);
+				PeerConnectRequestEvent connectRequest = new PeerConnectRequestEvent(0);
 				context().sendPacket(toPacket(connectRequest, PEER_ADDRESS));
 				data.setLastTriedTime(time);
 				data.incrementTimesTried();
--- src/main/java/context/connect/PeerConnectRequestEvent.java
+++ src/main/java/context/connect/PeerConnectRequestEvent.java
@@ -1,20 +1,21 @@
 package context.connect;
 
+import static context.data.user.LocalUser.LOCAL_USER;
+
 import java.io.Serializable;
 
 import common.event.NetworkEvent;
-import common.source.GameSource;
 
 public class PeerConnectRequestEvent extends NetworkEvent implements Serializable {
 
 	private static final long serialVersionUID = 3967611254630803156L;
 
-	public PeerConnectRequestEvent(GameSource source) {
-		super(source);
+	public PeerConnectRequestEvent() {
+		super(LOCAL_USER);
 	}
 
-	public PeerConnectRequestEvent(long time, GameSource source) {
-		super(time, source);
+	public PeerConnectRequestEvent(long time) {
+		super(time, LOCAL_USER);
 	}
 
 }
