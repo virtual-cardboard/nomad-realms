0b5d6f03a54f16d5c68ed62443e019e61208eecf
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-10-31 15:46:52 -0400

    Enlarge cards upon hover

--- src/main/java/context/game/NomadsGameInput.java
+++ src/main/java/context/game/NomadsGameInput.java
@@ -2,21 +2,53 @@ package context.game;
 
 import static common.event.NetworkEvent.fromPacket;
 
+import java.util.List;
+
 import common.GameInputEventHandler;
+import common.coordinates.IntCoordinates;
+import common.math.PosDim;
 import context.input.GameInput;
-import context.visuals.GameVisuals;
+import graphics.gui.CardDashboardGui;
+import graphics.gui.CardGui;
 
 public class NomadsGameInput extends GameInput {
+
+	private CardGui hovered;
 
 	@Override
 	protected void init() {
 		addPacketReceivedFunction(new GameInputEventHandler<>(event -> {
 			return fromPacket(event.model());
 		}));
-		addMouseReleasedFunction(new GameInputEventHandler<>(event -> {
-			GameVisuals visuals = context().visuals();
+		addMouseMovedFunction(new GameInputEventHandler<>(event -> {
+			NomadsGameVisuals visuals = (NomadsGameVisuals) context().visuals();
+			CardDashboardGui dashboardGui = visuals.getDashboardGui();
+			IntCoordinates cursorCoordinates = cursor().getCursorCoordinates();
+			List<CardGui> cardGuis = dashboardGui.cardGuis();
+			for (int i = 0; i < cardGuis.size(); i++) {
+				CardGui cardGui = cardGuis.get(i);
+				if (hoveringOver(cardGui, cursorCoordinates)) {
+					System.out.println("yay");
+					cardGui.hover();
+					hovered = cardGui;
+					return null;
+				} else {
+					cardGui.unhover();
+				}
+			}
+			if (hovered != null) {
+				System.out.println("Unhovered from " + hovered.getCard().name());
+			}
+			hovered = null;
 			return null;
 		}));
+	}
+
+	private boolean hoveringOver(CardGui cardGui, IntCoordinates cursor) {
+		PosDim pd = cardGui.posdim();
+		int cx = cursor.x;
+		int cy = cursor.y;
+		return pd.x + pd.w * 0.09f <= cx && cx <= pd.x + pd.w * 0.89f && pd.y + pd.h * 0.165f <= cy && cy <= pd.y + pd.h * 0.82f;
 	}
 
 }
--- src/main/java/graphics/gui/CardGui.java
+++ src/main/java/graphics/gui/CardGui.java
@@ -7,6 +7,7 @@ import context.GLContext;
 import context.ResourcePack;
 import context.visuals.gui.Gui;
 import context.visuals.gui.constraint.dimension.PixelDimensionConstraint;
+import context.visuals.gui.constraint.position.PixelPositionConstraint;
 import context.visuals.lwjgl.Texture;
 import context.visuals.renderer.TextRenderer;
 import context.visuals.renderer.TextureRenderer;
@@ -14,6 +15,10 @@ import context.visuals.text.GameFont;
 import model.card.GameCard;
 
 public class CardGui extends Gui {
+
+	public static final float WIDTH = 256;
+	public static final float HEIGHT = 512;
+	public static final float SIZE_FACTOR = 1.2f;
 
 	private GameCard card;
 	private TextureRenderer textureRenderer;
@@ -23,6 +28,7 @@ public class CardGui extends Gui {
 	private Texture front;
 	private Texture banner;
 	private GameFont font;
+	private boolean hovered;
 
 	public CardGui(GameCard card, TextureRenderer textureRenderer, TextRenderer textRenderer, ResourcePack resourcePack) {
 		this.card = card;
@@ -33,9 +39,8 @@ public class CardGui extends Gui {
 		front = resourcePack.getTexture("card_front");
 		banner = resourcePack.getTexture("card_banner");
 		font = resourcePack.getFont("baloo2");
-		setWidth(new PixelDimensionConstraint(256));
-		setHeight(new PixelDimensionConstraint(512));
-
+		setWidth(new PixelDimensionConstraint(WIDTH));
+		setHeight(new PixelDimensionConstraint(HEIGHT));
 	}
 
 	@Override
@@ -48,6 +53,34 @@ public class CardGui extends Gui {
 		textureRenderer.render(glContext, card.texture(), clone);
 		textRenderer.render(glContext, matrix4f.clone(), card.name(), x + width * 0.3f, y + height * 0.45f, width, font, width * 0.07f, colour(28, 68, 124));
 		textRenderer.render(glContext, matrix4f, card.text(), x + width * 0.21f, y + height * 0.52f, width * 0.58f, font, width * 0.06f, colour(28, 68, 124));
+	}
+
+	public void hover() {
+		if (!hovered) {
+			((PixelPositionConstraint) getPosX()).setPixels(((PixelPositionConstraint) getPosX()).getPixels() - WIDTH * (SIZE_FACTOR - 1) * 0.5f);
+			((PixelPositionConstraint) getPosY()).setPixels(((PixelPositionConstraint) getPosY()).getPixels() - HEIGHT * (SIZE_FACTOR - 1) * 0.5f);
+			((PixelDimensionConstraint) getWidth()).setPixels(WIDTH * SIZE_FACTOR);
+			((PixelDimensionConstraint) getHeight()).setPixels(HEIGHT * SIZE_FACTOR);
+			hovered = true;
+		}
+	}
+
+	public void unhover() {
+		if (hovered) {
+			((PixelPositionConstraint) getPosX()).setPixels(((PixelPositionConstraint) getPosX()).getPixels() + WIDTH * (SIZE_FACTOR - 1) * 0.5f);
+			((PixelPositionConstraint) getPosY()).setPixels(((PixelPositionConstraint) getPosY()).getPixels() + HEIGHT * (SIZE_FACTOR - 1) * 0.5f);
+			((PixelDimensionConstraint) getWidth()).setPixels(WIDTH);
+			((PixelDimensionConstraint) getHeight()).setPixels(HEIGHT);
+			hovered = false;
+		}
+	}
+
+	public boolean hovered() {
+		return hovered;
+	}
+
+	public GameCard getCard() {
+		return card;
 	}
 
 }
