b9bfa9336ad1bdd2b340b18fdcfbdcf3edc51dcd
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-11-19 11:19:16 -0500

    Zap effect

---
+++ src/main/java/context/game/visuals/handler/TookDamageSyncEventHandler.java
@@ -0,0 +1,9 @@
+package context.game.visuals.handler;
+
+public class TookDamageSyncEventHandler {
+
+	public TookDamageSyncEventHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/event/game/visualssync/TookDamageSyncEvent.java
@@ -0,0 +1,29 @@
+package event.game.visualssync;
+
+import model.actor.CardPlayer;
+import model.actor.HealthActor;
+
+public class TookDamageSyncEvent extends NomadRealmsVisualsSyncEvent {
+
+	private HealthActor target;
+	private int num;
+
+	public TookDamageSyncEvent(CardPlayer player, HealthActor target, int num) {
+		super(player);
+		this.target = target;
+		this.num = num;
+	}
+
+	public CardPlayer player() {
+		return (CardPlayer) source();
+	}
+
+	public HealthActor target() {
+		return target;
+	}
+
+	public int num() {
+		return num;
+	}
+
+}
