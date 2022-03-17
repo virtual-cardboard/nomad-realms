d834b33d71acd3e00fc220d189cf7120ef9a0997
Lunkle <bobbob.donny@gmail.com>
2022-03-17 07:55:42 -0400

    Moved loader graph into engine

--- src/main/java/loading/graph/Loader0.java
+++
@@ -1,7 +0,0 @@
-package loading.graph;
-
-public interface Loader0 {
-
-	public void load();
-
-}
--- src/main/java/loading/graph/Loader1.java
+++
@@ -1,9 +0,0 @@
-package loading.graph;
-
-import java.util.concurrent.Future;
-
-public interface Loader1<A> {
-
-	public void load(Future<A> a);
-
-}
