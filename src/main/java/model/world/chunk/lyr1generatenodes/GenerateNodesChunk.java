0af8e9ebd8d0be0624980861765e602cef6b2df3
JaryJay <47123100+JaryJay@users.noreply.github.com>
2022-03-26 21:17:56 -0400

    Use Random to generate nodes

--- src/main/java/model/world/chunk/lyr1generatenodes/GenerateNodesChunk.java
+++ src/main/java/model/world/chunk/lyr1generatenodes/GenerateNodesChunk.java
@@ -1,8 +1,8 @@
 package model.world.chunk.lyr1generatenodes;
 
+import java.util.Random;
+
 import common.math.Vector2i;
-import graphics.noise.OpenSimplexNoise;
-import model.world.WorldSeed;
 import model.world.chunk.AbstractTileChunk;
 import model.world.chunk.lyr0generatebiomes.GenerateBiomesChunk;
 import model.world.chunk.lyr2relocatenodes.RelocateNodesChunk;
@@ -10,9 +10,9 @@ import model.world.chunk.lyr3generateActors.ActorClusterNode;
 
 public class GenerateNodesChunk extends GenerateBiomesChunk {
 
-	private static final int NUM_NODES = 5;
-	private static final double MIN_RADIUS = 1;
-	private static final double MAX_RADIUS = 6;
+	public static final int NUM_NODES = 5;
+	public static final double MIN_RADIUS = 1;
+	public static final double MAX_RADIUS = 8;
 
 	protected ActorClusterNode[] nodes;
 
@@ -24,11 +24,13 @@ public class GenerateNodesChunk extends GenerateBiomesChunk {
 		GenerateNodesChunk c = new GenerateNodesChunk(pos);
 		prev.cloneDataTo(c);
 		c.nodes = new ActorClusterNode[NUM_NODES];
-		OpenSimplexNoise nodeSeed = new OpenSimplexNoise(WorldSeed.node(worldSeed));
+		long randSeed = ((long) pos.x << 32 | (pos.y & 0xffffffffL)) ^ worldSeed;
+		Random rand = new Random(randSeed);
 		for (int i = 0; i < NUM_NODES; i++) {
-			double x = (nodeSeed.eval(pos.x * 20000, pos.y * 20000, 20000 * i) * 0.5 + 0.5) * CHUNK_SIDE_LENGTH;
-			double y = (nodeSeed.eval(pos.x * 20000, pos.y * 20000, 20000 * i + 2000000) * 0.5 + 0.5) * CHUNK_SIDE_LENGTH;
-			double r = (nodeSeed.eval(pos.x * 20000, pos.y * 20000, 20000 * i + 4000000) * 0.5 + 0.5) * (MAX_RADIUS - MIN_RADIUS) + MIN_RADIUS;
+			double x = rand.nextDouble() * CHUNK_SIDE_LENGTH;
+			double y = rand.nextDouble() * CHUNK_SIDE_LENGTH;
+			double n = rand.nextDouble();
+			double r = Math.pow(2, 10 * (n - 1)) * (MAX_RADIUS - MIN_RADIUS) + MIN_RADIUS;
 			c.nodes[i] = new ActorClusterNode(x, y, r);
 		}
 		return c;
