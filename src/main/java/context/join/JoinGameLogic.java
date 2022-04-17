28f5313417e2a2d9b7d995f5ba88434e2b2e527a
Jay <jay.jrjren@gmail.com>
2022-04-16 21:27:53 -0400

    Refactor JoinGameLogic

--- src/main/java/context/join/JoinGameLogic.java
+++ src/main/java/context/join/JoinGameLogic.java
@@ -1,16 +1,9 @@
 package context.join;
 
-import static networking.ClientNetworkUtils.LOCAL_HOST;
-import static networking.ClientNetworkUtils.SERVER;
-
 import context.GameContext;
 import context.audio.GameAudio;
 import context.data.GameData;
-import context.game.NomadsGameAudio;
-import context.game.NomadsGameData;
-import context.game.NomadsGameInput;
-import context.game.NomadsGameLogic;
-import context.game.NomadsGameVisuals;
+import context.game.*;
 import context.input.GameInput;
 import context.input.networking.packet.address.PacketAddress;
 import context.logic.GameLogic;
@@ -19,6 +12,9 @@ import event.network.NomadRealmsNetworkEvent;
 import event.network.join.JoinClusterRequestEvent;
 import event.network.join.JoinClusterResponseEvent;
 import event.network.join.JoinEmptyClusterResponseEvent;
+
+import static networking.ClientNetworkUtils.LOCAL_HOST;
+import static networking.ClientNetworkUtils.SERVER;
 
 public final class JoinGameLogic extends GameLogic {
 
@@ -31,31 +27,33 @@ public final class JoinGameLogic extends GameLogic {
 		JoinClusterRequestEvent joinWorldRequestEvent = new JoinClusterRequestEvent(0, LOCAL_HOST.address(), data.username());
 		PacketAddress serverAddress = SERVER.address();
 		context().sendPacket(joinWorldRequestEvent.toPacket(serverAddress));
-		addHandler(NomadRealmsNetworkEvent.class, event -> System.out.println(event.getClass().getSimpleName()));
-		addHandler(JoinEmptyClusterResponseEvent.class, event -> {
-			GameAudio audio = new NomadsGameAudio();
-			GameData data = new NomadsGameData();
-			GameInput input = new NomadsGameInput();
-			GameLogic logic = new NomadsGameLogic(this.data.username());
-			GameVisuals visuals = new NomadsGameVisuals();
-			GameContext context = new GameContext(audio, data, input, logic, visuals);
-			context().transition(context);
-		});
-		addHandler(JoinClusterResponseEvent.class, event -> {
-		});
+		addHandler(JoinEmptyClusterResponseEvent.class, this::transitionToGame);
+		addHandler(JoinClusterResponseEvent.class, this::transitionToBootstrap);
+
+		addHandler(NomadRealmsNetworkEvent.class, this::printEvent);
 	}
 
 	@Override
 	public void update() {
 	}
 
-//	private void transitionToPeerConnect() {
-//		GameData data = new PeerConnectData(this.data.username());
-//		GameInput input = new PeerConnectInput();
-//		GameLogic logic = new PeerConnectLogic(this.data.response());
-//		GameVisuals visuals = new PeerConnectVisuals();
-//		GameContext context = new GameContext(new DefaultGameAudio(), data, input, logic, visuals);
-//		context().transition(context);
-//	}
+	private void transitionToGame(JoinEmptyClusterResponseEvent event) {
+		System.out.println("Transitioning to game");
+		GameAudio audio = new NomadsGameAudio();
+		GameData data = new NomadsGameData();
+		GameInput input = new NomadsGameInput();
+		GameLogic logic = new NomadsGameLogic(this.data.username());
+		GameVisuals visuals = new NomadsGameVisuals();
+		GameContext context = new GameContext(audio, data, input, logic, visuals);
+		context().transition(context);
+	}
+
+	private void transitionToBootstrap(JoinClusterResponseEvent event) {
+		System.out.println("Transitioning to bootstrap");
+	}
+
+	private void printEvent(NomadRealmsNetworkEvent event) {
+		System.out.println("Received " + event.getClass().getSimpleName() + " from " + event.source().address());
+	}
 
 }
