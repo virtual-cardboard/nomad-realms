655429447db1ce2c1be969b47efa1079c89f8504
Jay <jay.jrjren@gmail.com>
2022-04-23 21:22:15 -0400

    Change nomad art, fix CardResolvedEventVisualHandler

--- src/main/java/graphics/displayer/NomadDisplayer.java
+++ src/main/java/graphics/displayer/NomadDisplayer.java
@@ -1,21 +1,18 @@
 package graphics.displayer;
 
-import static context.visuals.colour.Colour.rgb;
-
 import app.NomadsSettings;
 import context.GLContext;
 import context.ResourcePack;
 import context.game.visuals.GameCamera;
-import context.visuals.colour.Colour;
+import context.visuals.lwjgl.Texture;
 import engine.common.math.Vector2f;
-import graphics.displayable.LimbBodyPart;
-import graphics.displayable.TextureBodyPart;
 import model.actor.Nomad;
 import model.state.GameState;
 
 public class NomadDisplayer extends CardPlayerDisplayer<Nomad> {
 
 	private Vector2f lastDirection = new Vector2f(0, 1);
+	private Texture nomadTexture;
 
 	public NomadDisplayer(long nomadID) {
 		super(nomadID);
@@ -24,86 +21,26 @@ public class NomadDisplayer extends CardPlayerDisplayer<Nomad> {
 	@Override
 	protected void init(ResourcePack resourcePack, GameState state) {
 		super.init(resourcePack, state);
-		TextureBodyPart head = new TextureBodyPart(resourcePack.getTexture("nomad_head"), 0.3f);
-		head.height = 110;
-		head.dist = 5;
-		head.minScale = 0.9f;
-		LimbBodyPart leftEye = new LimbBodyPart(4, 4, rgb(64, 44, 15));
-		leftEye.rot = 0.5f;
-		leftEye.height = 35;
-		leftEye.dist = 20;
-		leftEye.pointHeight = 27;
-		leftEye.pointDist = 20;
-		leftEye.armWidth = 8;
-		leftEye.foreArmWidth = 8;
-		LimbBodyPart rightEye = new LimbBodyPart(4, 4, rgb(64, 44, 15));
-		rightEye.rot = -0.5f;
-		rightEye.height = 35;
-		rightEye.dist = 20;
-		rightEye.pointHeight = 27;
-		rightEye.pointDist = 20;
-		rightEye.armWidth = 8;
-		rightEye.foreArmWidth = 8;
-		TextureBodyPart body = new TextureBodyPart(resourcePack.getTexture("nomad_body"), 0.3f);
-		body.height = -60;
-		body.dist = -10;
-		body.minScale = 0.75f;
-		LimbBodyPart leftArm = new LimbBodyPart(10, 10, rgb(64, 44, 15));
-		leftArm.rot = 1.8f;
-		leftArm.height = -4;
-		leftArm.dist = 35;
-		leftArm.pointHeight = -45;
-		leftArm.pointDist = 42;
-		leftArm.armWidth = 10;
-		leftArm.foreArmWidth = 10;
-		LimbBodyPart rightArm = new LimbBodyPart(10, 10, rgb(64, 44, 15));
-		rightArm.rot = -1.8f;
-		rightArm.height = -4;
-		rightArm.dist = 35;
-		rightArm.pointHeight = -45;
-		rightArm.pointDist = 42;
-		rightArm.armWidth = 10;
-		rightArm.foreArmWidth = 10;
-		LimbBodyPart leftLeg = new LimbBodyPart(10, 10, rgb(64, 44, 15));
-		leftLeg.rot = 1.8f;
-		leftLeg.height = -40;
-		leftLeg.dist = 25;
-		leftLeg.pointHeight = -79.95f;
-		leftLeg.pointDist = 25f;
-		leftLeg.armWidth = 10;
-		leftLeg.foreArmWidth = 10;
-		LimbBodyPart rightLeg = new LimbBodyPart(10, 10, rgb(64, 44, 15));
-		rightLeg.rot = -1.8f;
-		rightLeg.height = -40;
-		rightLeg.dist = 25;
-		rightLeg.pointHeight = -79.95f;
-		rightLeg.pointDist = 25f;
-		rightLeg.armWidth = 10;
-		rightLeg.foreArmWidth = 10;
-		addBodyPart(head);
-		addBodyPart(leftEye);
-		addBodyPart(rightEye);
-		addBodyPart(body);
-		addBodyPart(leftArm);
-		addBodyPart(rightArm);
-		addBodyPart(leftLeg);
-		addBodyPart(rightLeg);
+		nomadTexture = resourcePack.getTexture("actor_jary");
 	}
 
 	@Override
 	public void display(GLContext glContext, NomadsSettings s, GameState state, GameCamera camera, float alpha) {
 		Nomad nomad = (Nomad) state.cardPlayer(actorID());
 //		lastDirection = lastDirection.add(nomad.direction().scale(0.2f)).normalise();
-		displayHealth(glContext, s, nomad, state, camera);
+//		displayHealth(glContext, s, nomad, state, camera);
 //		displayQueue(glContext, s, nomad, state, camera);
 		displayEffectChains(glContext, s, nomad, state, camera);
-		displayBodyParts(glContext, s, state, camera, nomad, alpha, lastDirection);
+//		displayBodyParts(glContext, s, state, camera, nomad, alpha, lastDirection);
+		Vector2f sp = nomad.screenPos(camera, s);
+		float side = s.worldScale * 1.1f;
+		textureRenderer.render(nomadTexture, sp.x - side / 2, sp.y - side * 0.8f, side, side);
 		long longID = nomad.longID();
 		Vector2f screenPos = nomad.screenPos(camera, s);
-		float x = screenPos.x - 60;
-		float y = screenPos.y - 120;
-		textRenderer.alignCenter();
-		textRenderer.render(x, y, "P" + longID, 120, font, 30, Colour.rgb(69, 165, 255));
+//		float x = screenPos.x - 60;
+//		float y = screenPos.y - 120;
+//		textRenderer.alignCenter();
+//		textRenderer.render(x, y, "P" + longID, 120, font, 30, Colour.rgb(69, 165, 255));
 	}
 
 }
