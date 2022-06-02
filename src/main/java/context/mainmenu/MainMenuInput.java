10c0e4cf7c42d1192643168e145af05ab4636ef4
JaryJay <jay.jrjren@gmail.com>
2022-06-02 19:02:23 -0400

    Fix compile errors

--- src/main/java/context/mainmenu/MainMenuInput.java
+++ src/main/java/context/mainmenu/MainMenuInput.java
@@ -1,15 +1,6 @@
 package context.mainmenu;
 
-import context.GameContext;
-import context.audio.DefaultGameAudio;
-import context.data.GameData;
 import context.input.GameInput;
-import context.join.JoinGameData;
-import context.join.JoinGameInput;
-import context.join.JoinGameLogic;
-import context.join.JoinGameVisuals;
-import context.logic.GameLogic;
-import context.visuals.GameVisuals;
 
 public class MainMenuInput extends GameInput {
 
@@ -17,15 +8,6 @@ public class MainMenuInput extends GameInput {
 	protected void init() {
 		MainMenuVisuals visuals = (MainMenuVisuals) context().visuals();
 		addMousePressedFunction(new StartButtonMousePressedFunction(visuals, this));
-	}
-
-	public void transition() {
-		GameData data = new JoinGameData();
-		GameInput input = new JoinGameInput();
-		GameLogic logic = new JoinGameLogic();
-		GameVisuals visuals = new JoinGameVisuals();
-		GameContext context = new GameContext(new DefaultGameAudio(), data, input, logic, visuals);
-		context().transition(context);
 	}
 
 }
