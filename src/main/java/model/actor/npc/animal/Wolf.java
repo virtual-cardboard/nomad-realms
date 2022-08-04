5e2f5ac3d87c357370b57301667d35386156f70c
PizzaPrix <seanwaterloo2997@gmail.com>
2022-08-03 20:23:53 -0400

    Create Wolf class

---
+++ src/main/java/model/actor/npc/animal/Wolf.java
@@ -0,0 +1,43 @@
+package model.actor.npc.animal;
+
+import static model.item.Item.MEAT;
+
+import graphics.displayer.WolfDisplayer;
+import model.actor.CardPlayerSerializationFormats;
+import model.actor.NpcActor;
+import model.item.ItemCollection;
+
+/**
+ * A Wolf
+ *
+ * @author Jay
+ */
+public class Wolf extends NpcActor {
+
+    public Wolf() {
+        super(3);
+        setDisplayer(new WolfDisplayer());
+        this.ai = new WolfAi();
+    }
+
+    public Wolf(long id, WolfDisplayer displayer) {
+        super(3, id);
+        setDisplayer(displayer);
+    }
+
+    @Override
+    public Wolf copy() {
+        return super.copyTo(new Wolf(id, (WolfDisplayer) displayer()));
+    }
+
+    @Override
+    public ItemCollection dropItems() {
+        return new ItemCollection(MEAT, 1);
+    }
+
+    @Override
+    public CardPlayerSerializationFormats formatEnum() {
+        return null; // TODO
+    }
+
+}
