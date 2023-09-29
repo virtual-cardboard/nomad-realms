66bb38957b64bd08348b14e3dd01a59a8c4e198c
Lunkle <bobbob.donny@gmail.com>
2023-09-28 21:31:59 -0400

    Rename Client to App

--- core/src/com/nomadrealms/NomadRealmsApp.java
+++ core/src/com/nomadrealms/NomadRealmsApp.java
@@ -5,27 +5,29 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class NomadRealmsClient extends ApplicationAdapter {
+public class NomadRealmsApp extends ApplicationAdapter {
+
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("lava-golem.png");
 	}
 
 	@Override
-	public void render () {
+	public void render() {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
 		img.dispose();
 	}
+
 }
