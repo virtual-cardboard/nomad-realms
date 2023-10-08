3d21c5c9bddee435a1c43dc8bf084138b551e59d
Lunkle <bobbob.donny@gmail.com>
2023-10-08 09:06:10 -0400

    Hexagon rendering

--- core/src/com/nomadrealms/NomadRealmsApp.java
+++ core/src/com/nomadrealms/NomadRealmsApp.java
@@ -3,10 +3,12 @@ package com.nomadrealms;
 import static com.nomadrealms.loading.LocalAssets.disposeLocalAssets;
 import static com.nomadrealms.loading.LocalAssets.lavaGolemTexture;
 import static com.nomadrealms.loading.LocalAssets.loadLocalAssets;
+import static com.nomadrealms.rendering.basic.shape.HexagonShapeRenderer.renderHexagon;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.nomadrealms.rendering.basic.shape.HexagonShapeRenderer;
 
 public class NomadRealmsApp extends ApplicationAdapter {
 
@@ -16,6 +18,7 @@ public class NomadRealmsApp extends ApplicationAdapter {
 	public void create() {
 		batch = new SpriteBatch();
 		loadLocalAssets();
+		HexagonShapeRenderer.init();
 	}
 
 	@Override
@@ -24,6 +27,9 @@ public class NomadRealmsApp extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(lavaGolemTexture, 10, 20);
 		batch.end();
+
+		renderHexagon(100, 100, 50);
+
 	}
 
 	@Override
