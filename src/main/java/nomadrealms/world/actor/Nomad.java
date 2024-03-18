f5e7adc74dc7bcc833dcdfa287d017a9f75b1c7d
Lunkle <bobbob.donny@gmail.com>
2024-03-17 21:57:54 -0400

    Nomad name

--- src/main/java/nomadrealms/world/actor/Nomad.java
+++ src/main/java/nomadrealms/world/actor/Nomad.java
@@ -1,31 +1,49 @@
 package nomadrealms.world.actor;
 
-import common.math.Vector2f;
+import static common.colour.Colour.*;
+import static nomadrealms.world.map.tile.Tile.SCALE;
+
 import nomadrealms.render.RenderingEnvironment;
-import nomadrealms.world.map.Chunk;
+import nomadrealms.world.map.tile.Tile;
 import visuals.lwjgl.render.framebuffer.DefaultFrameBuffer;
 
 public class Nomad {
 
-	private int x, y;
+	private String name;
+	private Tile tile;
 	private int health;
 
-	public Nomad(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public Nomad(String name, Tile tile) {
+		this.name = name;
+		this.tile = tile;
 		this.health = 10;
 	}
 
 	public void render(RenderingEnvironment re) {
+		float scale = 0.6f * SCALE;
 		DefaultFrameBuffer.instance().render(
 				() -> {
 					re.textureRenderer.render(
 							re.imageMap.get("nomad"),
-							0, 0,
-							100, 100
+							tile.getScreenPosition().x() - 0.5f * scale,
+							tile.getScreenPosition().y() - 0.7f * scale,
+							scale, scale
+					);
+					re.textRenderer.render(
+							tile.getScreenPosition().x(),
+							tile.getScreenPosition().y() + 0.1f * scale,
+							name,
+							0,
+							re.font,
+							0.3f * scale,
+							rgb(255, 255, 255)
 					);
 				}
 		);
+	}
+
+	public void tile(Tile tile) {
+		this.tile = tile;
 	}
 
 }
