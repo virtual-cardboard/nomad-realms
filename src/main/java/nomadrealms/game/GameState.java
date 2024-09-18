8418bc94de5dd05617d337606a97246a1cf2f2d0
Lunkle <bobbob.donny@gmail.com>
2024-09-17 23:37:54 -0400

    Tile selection with mouse

--- src/main/java/nomadrealms/game/GameState.java
+++ src/main/java/nomadrealms/game/GameState.java
@@ -1,20 +1,18 @@
 package nomadrealms.game;
 
+import static nomadrealms.game.world.map.area.coordinate.TileCoordinate.tileCoordinateOf;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 
 import common.math.Vector2f;
-import common.math.Vector2i;
 import context.input.Mouse;
 import nomadrealms.game.event.InputEvent;
 import nomadrealms.game.event.InputEventFrame;
 import nomadrealms.game.world.World;
 import nomadrealms.game.world.map.area.Tile;
-import nomadrealms.game.world.map.area.coordinate.ChunkCoordinate;
-import nomadrealms.game.world.map.area.coordinate.RegionCoordinate;
 import nomadrealms.game.world.map.area.coordinate.TileCoordinate;
-import nomadrealms.game.world.map.area.coordinate.ZoneCoordinate;
 import nomadrealms.render.RenderingEnvironment;
 
 /**
@@ -53,20 +51,12 @@ public class GameState {
 
 	public Tile getMouseHexagon(Mouse mouse) {
 		Vector2f cameraPosition = new Vector2f(0, 0);
-//		Vector2i coord = screenToTile(cameraPosition.add(mouse.coordinate().value().toVector()));
-		// TODO first calculate which chunk the mouse is on
-		Vector2i coord = new Vector2i(0, 0);
-		return world.getTile(new TileCoordinate(new ChunkCoordinate(new ZoneCoordinate(new RegionCoordinate(0, 0), 0,
-				0), 0, 0), coord.x(), coord.y()));
-	}
-
-	private Vector2f calculatePos(Vector2i cursor) {
-		float chunkWidth = 100000; //inputInfo.settings.chunkWidth();
-		float chunkHeight = 100000; //inputInfo.settings.chunkHeight();
-		// The position in pixels from the top left corner of the current chunk
-		float posX = (cursor.x() % chunkWidth + chunkWidth) % chunkWidth;
-		float posY = (cursor.y() % chunkHeight + chunkHeight) % chunkHeight;
-		return new Vector2f(posX, posY);
+		TileCoordinate coord = tileCoordinateOf(cameraPosition.add(mouse.coordinate().value().toVector()));
+		System.out.println("========");
+		System.out.println("Mouse: " + mouse.coordinate().value().toVector());
+		System.out.println("Chunk: " + coord.chunk());
+		System.out.println("Tile: " + coord);
+		return world.getTile(coord);
 	}
 
 	public InputEventFrame lastInputFrame() {
--- src/main/java/nomadrealms/game/world/map/area/coordinate/TileCoordinate.java
+++ src/main/java/nomadrealms/game/world/map/area/coordinate/TileCoordinate.java
@@ -1,7 +1,17 @@
 package nomadrealms.game.world.map.area.coordinate;
 
 import static common.math.MathUtil.posMod;
+import static nomadrealms.game.world.map.area.Tile.TILE_HORIZONTAL_SPACING;
+import static nomadrealms.game.world.map.area.Tile.TILE_RADIUS;
+import static nomadrealms.game.world.map.area.Tile.TILE_VERTICAL_SPACING;
 import static nomadrealms.game.world.map.area.coordinate.ChunkCoordinate.CHUNK_SIZE;
+import static nomadrealms.game.world.map.area.coordinate.ChunkCoordinate.chunkCoordinateOf;
+import static nomadrealms.game.world.map.area.coordinate.RegionCoordinate.REGION_SIZE;
+import static nomadrealms.game.world.map.area.coordinate.ZoneCoordinate.ZONE_SIZE;
+import static nomadrealms.render.vao.shape.HexagonVao.HEIGHT;
+import static nomadrealms.render.vao.shape.HexagonVao.SIDE_LENGTH;
+
+import common.math.Vector2f;
 
 public class TileCoordinate extends Coordinate {
 
@@ -80,6 +90,77 @@ public class TileCoordinate extends Coordinate {
 		return new TileCoordinate(chunkCoord, tileX, tileY);
 	}
 
+	public static TileCoordinate tileCoordinateOf(Vector2f position) {
+		ChunkCoordinate chunkCoord = chunkCoordinateOf(position);
+		Vector2f offset = new Vector2f()
+				.add(new Vector2f(chunkCoord.region().x(), chunkCoord.region().y()).scale(REGION_SIZE))
+				.add(new Vector2f(chunkCoord.zone().x(), chunkCoord.zone().y()).scale(ZONE_SIZE))
+				.add(new Vector2f(chunkCoord.x(), chunkCoord.y()).scale(CHUNK_SIZE))
+				.scale(TILE_HORIZONTAL_SPACING, TILE_VERTICAL_SPACING)
+				.sub(position).negate();
+
+
+		float quarterWidth = TILE_RADIUS * SIDE_LENGTH / 2;
+		float halfHeight = TILE_RADIUS * HEIGHT;
+
+		int tileX = (int) (offset.x() / TILE_HORIZONTAL_SPACING);
+		int tileY;
+		if (offset.x() % TILE_HORIZONTAL_SPACING >= quarterWidth) {
+			// In center rectangle of hexagon
+			if (tileX % 2 == 0) {
+				// Not shifted
+				tileY = (int) (offset.y() / TILE_VERTICAL_SPACING);
+			} else {
+				// Shifted
+				tileY = (int) ((offset.y() - halfHeight) / TILE_VERTICAL_SPACING);
+			}
+		} else {
+			// Beside the zig-zag
+			float xOffset;
+			if ((int) (offset.x() / TILE_HORIZONTAL_SPACING) % 2 == 0) {
+				// Zig-zag starting from right side
+				xOffset = quarterWidth * Math.abs(offset.y() % TILE_VERTICAL_SPACING - halfHeight) / halfHeight;
+			} else {
+				// Zig-zag starting from left side
+				xOffset = quarterWidth * Math.abs((offset.y() + halfHeight) % TILE_VERTICAL_SPACING - halfHeight) / halfHeight;
+			}
+			if (offset.x() % TILE_HORIZONTAL_SPACING <= xOffset) {
+				// Left of zig-zag
+				tileX--;
+			}
+			if (tileX % 2 == 0) {
+				// Not shifted
+				tileY = (int) (offset.y() / TILE_VERTICAL_SPACING);
+			} else {
+				// Shifted
+				tileY = (int) ((offset.y() - halfHeight) / TILE_VERTICAL_SPACING);
+			}
+		}
+
+		return new TileCoordinate(chunkCoord, tileX, tileY).normalize();
+	}
+
+	/**
+	 * Run this if you want to fix coordinates that are out of bound instead of doing the tough math to prevent it
+	 * from happening in the first place. Calling this is probably a bug waiting to happen.
+	 */
+	public TileCoordinate normalize() {
+		int x = posMod(x(), CHUNK_SIZE);
+		int y = posMod(y(), CHUNK_SIZE);
+		ChunkCoordinate chunkCoord = chunk;
+		if (x() < 0) {
+			chunkCoord = chunkCoord.left();
+		} else if (x() >= CHUNK_SIZE) {
+			chunkCoord = chunkCoord.right();
+		}
+		if (y() < 0) {
+			chunkCoord = chunkCoord.up();
+		} else if (y() >= CHUNK_SIZE) {
+			chunkCoord = chunkCoord.down();
+		}
+		return new TileCoordinate(chunkCoord, x, y);
+	}
+
 	@Override
 	public RegionCoordinate region() {
 		return chunk.region();
@@ -100,6 +181,14 @@ public class TileCoordinate extends Coordinate {
 			return x() == other.x() && y() == other.y() && chunk.equals(other.chunk);
 		}
 		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "Tile{" +
+				"x=" + x() + ", " +
+				"y=" + y() +
+				'}';
 	}
 
 }
