3da3e04f472f727e1320d7dcfe8b28e0f458197e
Lunkle <bobbob.donny@gmail.com>
2024-11-16 22:01:57 -0500

    Biome types

---
+++ src/main/java/nomadrealms/game/world/map/generation/status/biome/BiomeType.java
@@ -0,0 +1,17 @@
+package nomadrealms.game.world.map.generation.status.biome;
+
+public enum BiomeType {
+
+	UNDEFINED,
+	GRASSLAND,
+	FOREST,
+	DESERT,
+	SNOW,
+	TUNDRA,
+	SWAMP,
+	BEACH,
+	OCEAN,
+	DEEP_OCEAN,
+	VOID;
+
+}
