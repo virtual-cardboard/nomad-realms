27c16d5e1c876286a95671ddeb1584021bfd099a
Jay <jay.jrjren@gmail.com>
2024-12-08 15:46:15 -0500

    Turn GameCard into an enum

--- src/main/java/nomadrealms/game/card/GameCard.java
+++ src/main/java/nomadrealms/game/card/GameCard.java
@@ -1,89 +1,102 @@
 package nomadrealms.game.card;
 
+import static nomadrealms.game.card.target.TargetType.CARD_PLAYER;
+import static nomadrealms.game.card.target.TargetType.HEXAGON;
+import static nomadrealms.game.card.target.TargetType.NONE;
+
 import nomadrealms.game.actor.structure.factory.StructureType;
-import nomadrealms.game.card.expression.*;
+import nomadrealms.game.card.expression.BuryAnySeedExpression;
+import nomadrealms.game.card.expression.CardExpression;
+import nomadrealms.game.card.expression.CreateStructureExpression;
+import nomadrealms.game.card.expression.DamageExpression;
+import nomadrealms.game.card.expression.EditTileExpression;
+import nomadrealms.game.card.expression.GatherExpression;
+import nomadrealms.game.card.expression.MoveExpression;
+import nomadrealms.game.card.expression.SelfHealExpression;
 import nomadrealms.game.card.target.TargetingInfo;
 import nomadrealms.game.world.map.tile.factory.TileType;
 
-import static nomadrealms.game.card.target.TargetType.*;
+public enum GameCard implements Card {
 
-public class GameCard implements Card {
+	MEANDER(
+			"Meander",
+			"Move to target hexagon",
+			new MoveExpression(),
+			new TargetingInfo(HEXAGON, 1)),
+	ATTACK(
+			"Attack",
+			"Deal 2 to target character",
+			new DamageExpression(2),
+			new TargetingInfo(CARD_PLAYER, 1)),
+	MOVE(
+			"Move",
+			"Move to target hexagon",
+			new MoveExpression(),
+			new TargetingInfo(HEXAGON, 2)),
+	HEAL(
+			"Heal",
+			"Restore 2 to self",
+			new SelfHealExpression(2),
+			new TargetingInfo(NONE, 10)),
+	TILL_SOIL(
+			"Till Soil",
+			"Till the current tile",
+			new EditTileExpression(TileType.SOIL),
+			new TargetingInfo(HEXAGON, 10)),
+	PLANT_SEED(
+			"Plant seed",
+			"Plant a seed on current tile",
+			new BuryAnySeedExpression(),
+			new TargetingInfo(NONE, 10)),
+	GATHER(
+			"Gather",
+			"Gather items on current tile",
+			new GatherExpression(1),
+			new TargetingInfo(NONE, 1)),
+	CREATE_ROCK(
+			"Create Rock",
+			"Create a rock on target tile",
+			new CreateStructureExpression(StructureType.ROCK),
+			new TargetingInfo(HEXAGON, 1)),
+	ELECTROSTATIC_ZAPPER(
+			"Electrostatic Zapper",
+			"Whenever a card is played within range 5, deal 2 to the source",
+			new CreateStructureExpression(StructureType.ELECTROSTATIC_ZAPPER),
+			new TargetingInfo(HEXAGON, 1));
 
-    public static final GameCard MOVE = new GameCard(
-            "Move",
-            "Move to target hexagon",
-            new MoveExpression(),
-            new TargetingInfo(HEXAGON, 10));
-    public static final GameCard ATTACK = new GameCard(
-            "Attack",
-            "Deal 2 to target character",
-            new DamageExpression(2),
-            new TargetingInfo(CARD_PLAYER, 10));
-    public static final GameCard HEAL = new GameCard(
-            "Heal",
-            "Restore 2 to self",
-            new SelfHealExpression(2),
-            new TargetingInfo(NONE, 10));
-    public static final GameCard TILL_SOIL = new GameCard(
-            "Till Soil",
-            "Till the current tile",
-            new EditTileExpression(TileType.SOIL),
-            new TargetingInfo(HEXAGON, 10));
-    public static final GameCard PLANT_SEED = new GameCard(
-            "Plant seed",
-            "Plant a seed on current tile",
-            new BuryAnySeedExpression(),
-            new TargetingInfo(NONE, 10));
-    public static final GameCard GATHER = new GameCard(
-            "Gather",
-            "Gather items on current tile",
-            new GatherExpression(1),
-            new TargetingInfo(NONE, 1));
-    public static final GameCard CREATE_ROCK = new GameCard(
-            "Create Rock",
-            "Create a rock on target tile",
-            new CreateStructureExpression(StructureType.ROCK),
-            new TargetingInfo(HEXAGON, 1)
-    );
-    public static final GameCard ELECTROSTATIC_ZAPPER = new GameCard(
-            "Electrostatic Zapper",
-            "Whenever a card is played within range 5, deal 2 to the source",
-            new CreateStructureExpression(StructureType.ELECTROSTATIC_ZAPPER),
-            new TargetingInfo(HEXAGON, 1)
-    );
+	private final String title;
+	private final String description;
+	private final CardExpression expression;
+	private final TargetingInfo targetingInfo;
 
-    private final String name;
-    private final String description;
-    private final CardExpression expression;
-    private final TargetingInfo targetingInfo;
+	private GameCard(String name, String description, CardExpression expression, TargetingInfo targetingInfo) {
+		this.title = name;
+		this.description = description;
+		this.expression = expression;
+		this.targetingInfo = targetingInfo;
+	}
 
-    public GameCard(String name, String description, CardExpression expression, TargetingInfo targetingInfo) {
-        this.name = name;
-        this.description = description;
-        this.expression = expression;
-        this.targetingInfo = targetingInfo;
-    }
+	public String title() {
+		return title;
+	}
 
-    public String name() {
-        return name;
-    }
+	public String description() {
+		return description;
+	}
 
-    public String description() {
-        return description;
-    }
+	public CardExpression expression() {
+		return expression;
+	}
 
-    public CardExpression expression() {
-        return expression;
-    }
+	public TargetingInfo targetingInfo() {
+		return targetingInfo;
+	}
 
-    public TargetingInfo targetingInfo() {
-        return targetingInfo;
-    }
+	@Override
+	public String toString() {
+		return "{" +
+				"name='" + title +
+				'}';
+	}
 
-    @Override
-    public String toString() {
-        return "{" +
-                "name='" + name +
-                '}';
-    }
 }
