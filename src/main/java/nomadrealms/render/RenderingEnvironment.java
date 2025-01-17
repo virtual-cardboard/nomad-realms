6ea5262a61b03b15516ae12ac9fd49cce7129ddc
Lunkle <bobbob.donny@gmail.com>
2025-01-16 21:00:55 -0500

    Feral monkey image

--- src/main/java/nomadrealms/render/RenderingEnvironment.java
+++ src/main/java/nomadrealms/render/RenderingEnvironment.java
@@ -61,24 +61,25 @@ public class RenderingEnvironment {
 	}
 
 	private void loadShaders() {
-		defaultVertexShader =
-				new VertexShader().source(readFileAsString(getFile("/shaders/defaultVertex.glsl"))).load();
-		defaultFragmentShader =
-				new FragmentShader().source(readFileAsString(getFile("/shaders/defaultFrag.glsl"))).load();
+		defaultVertexShader = new VertexShader().source(readFileAsString(getFile("/shaders/defaultVertex.glsl")))
+				.load();
+		defaultFragmentShader = new FragmentShader().source(readFileAsString(getFile("/shaders/defaultFrag.glsl")))
+				.load();
 		defaultShaderProgram = new ShaderProgram().attach(defaultVertexShader, defaultFragmentShader).load();
 	}
 
 	private void loadImages() {
 		imageMap.put("nomad", new Texture().image(loadImage(getFile("/images/nomad.png"))).load());
 		imageMap.put("farmer", new Texture().image(loadImage(getFile("/images/farmer.png"))).load());
-		imageMap.put("feral_monkey", new Texture().image(loadImage(getFile("/images/oak_log.png"))).load());
+		imageMap.put("feral_monkey", new Texture().image(loadImage(getFile("/images/feral_monkey.png"))).load());
 		imageMap.put("oak_log", new Texture().image(loadImage(getFile("/images/oak_log.png"))).load());
 		imageMap.put("wheat_seed", new Texture().image(loadImage(getFile("/images/wheat_seed.png"))).load());
 		imageMap.put("rock_1", new Texture().image(loadImage(getFile("/images/rock_1.png"))).load());
 		imageMap.put("tree_1", new Texture().image(loadImage(getFile("/images/tree_1.png"))).load());
 		imageMap.put("fence", new Texture().image(loadImage(getFile("/images/fence.png"))).load());
 		imageMap.put("chest", new Texture().image(loadImage(getFile("/images/chest.png"))).load());
-		imageMap.put("electrostatic_zapper", new Texture().image(loadImage(getFile("/images/electrostatic_zapper.png"))).load());
+		imageMap.put("electrostatic_zapper",
+				new Texture().image(loadImage(getFile("/images/electrostatic_zapper.png"))).load());
 	}
 
 	private File getFile(String name) {
