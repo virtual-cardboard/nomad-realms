109b254fc5ec035fb672a09469a7cfdc5142e77c
Lunkle <d.ren.donny@gmail.com>
2025-11-18 23:31:38 -0500

    Refactor tile color initialization for Grass and Water
    Simplified GrassTile and WaterTile constructors to use a base color and generate an alternate shade by reducing RGB values. This improves code reuse and consistency in color variation logic between tile types.
    
    Update CardQueueTest to use GrassTile
    
    Replaces instantiation of Tile with GrassTile in CardQueueTest to match expected parameters and improve test accuracy.
    
    Update GameMapTest.java

--- src/main/java/nomadrealms/context/game/world/map/tile/GrassTile.java
+++ src/main/java/nomadrealms/context/game/world/map/tile/GrassTile.java
@@ -1,5 +1,8 @@
 package nomadrealms.context.game.world.map.tile;
 
+import static engine.common.colour.Colour.b;
+import static engine.common.colour.Colour.g;
+import static engine.common.colour.Colour.r;
 import static engine.common.colour.Colour.rgb;
 import static nomadrealms.context.game.world.map.tile.factory.TileType.GRASS;
 
@@ -17,15 +20,13 @@ public class GrassTile extends Tile {
 	}
 
 	public GrassTile(Chunk chunk, TileCoordinate coord) {
-		super(chunk, coord);
-		int color1 = rgb(126, 200, 80);
-		int color2 = rgb(110, 180, 70);
-		this.color = (coord.x() + coord.y()) % 2 == 0 ? color1 : color2;
+		this(chunk, coord, rgb(126, 200, 80));
 	}
 
-	public GrassTile(Chunk chunk, TileCoordinate coord, int color) {
+	public GrassTile(Chunk chunk, TileCoordinate coord, int rgb) {
 		super(chunk, coord);
-		this.color = color;
+		int alt = rgb((int) (r(rgb) * 0.9f), (int) (g(rgb) * 0.9f), (int) (b(rgb) * 0.9f));
+		this.color = (coord.x() + coord.y()) % 2 == 0 ? rgb : alt;
 	}
 
 	@Override
