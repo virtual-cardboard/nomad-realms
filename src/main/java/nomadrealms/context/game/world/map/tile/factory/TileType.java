39cd4a99bbf04ade83d53eb52e71c730545f64d5
Lunkle <d.ren.donny@gmail.com>
2025-11-18 22:56:41 -0500

    Add VOID tile type and support for void tiles
    Introduces a new VOID tile type and its implementation (VoidTile). Updates tile factory and generation strategy to handle VOID tiles, which are now used for out-of-bounds or unspecified map areas. Adjusts predefined world data to include VOID tiles at map edges.
    
    Update VoidTile.java

--- src/main/java/nomadrealms/context/game/world/map/tile/factory/TileType.java
+++ src/main/java/nomadrealms/context/game/world/map/tile/factory/TileType.java
@@ -1,5 +1,5 @@
 package nomadrealms.context.game.world.map.tile.factory;
 
 public enum TileType {
-	GRASS, WATER, ICE, SOIL, SAND, SNOW, STONE, GRAYSCALE
+	VOID, GRASS, WATER, ICE, SOIL, SAND, SNOW, STONE, GRAYSCALE
 }
