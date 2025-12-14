eaf89400e784353a0099c00edda29ade2e6c3288
Lunkle <d.ren.donny@gmail.com>
2025-12-14 17:00:22 -0500

    Refactor TextureRenderer and update BlurContext usage
    Removed the matrix4f parameter from TextureRenderer.render and moved uniform setting responsibilities to the caller. Updated BlurContext to set shader uniforms directly before rendering, improving separation of concerns and flexibility.

--- src/main/java/engine/visuals/rendering/texture/TextureRenderer.java
+++ src/main/java/engine/visuals/rendering/texture/TextureRenderer.java
@@ -100,13 +100,9 @@ public class TextureRenderer {
 		vao.draw(glContext);
 	}
 
-	public void render(Texture texture, Matrix4f matrix4f, ShaderProgram program) {
+	public void render(Texture texture, ShaderProgram program) {
 		program.use(glContext);
 		texture.bind();
-		program.uniforms()
-				.set("transform", matrix4f)
-				.set("textureSampler", 0)
-				.complete();
 		vao.draw(glContext);
 	}
 
