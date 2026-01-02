a8ceee2ea0b59bd10cc1e337ba50cbc2bf52ffce
Lunkle <d.ren.donny@gmail.com>
2026-01-01 23:09:35 -0500

    Swap out tree image

--- src/main/java/nomadrealms/context/game/actor/structure/TreeStructure.java
+++ src/main/java/nomadrealms/context/game/actor/structure/TreeStructure.java
@@ -2,8 +2,8 @@ package nomadrealms.context.game.actor.structure;
 
 public class TreeStructure extends Structure {
 
-    public TreeStructure() {
-        super("tree", "tree_1", 1, 50);
-    }
+	public TreeStructure() {
+		super("tree", "oak_tree", 1, 50);
+	}
 
 }
--- src/main/java/nomadrealms/render/RenderingEnvironment.java
+++ src/main/java/nomadrealms/render/RenderingEnvironment.java
@@ -125,7 +125,10 @@ public class RenderingEnvironment {
 		imageMap.put("rock_1", new Texture().image(loadImage(getFile("/images/rock_1.png"))).load());
 		imageMap.put("tree_1", new Texture().image(loadImage(getFile("/images/tree_1.png"))).load());
 		imageMap.put("fence", new Texture().image(loadImage(getFile("/images/fence.png"))).load());
+		imageMap.put("oak_tree", new Texture().image(loadImage(getFile("/images/oak_tree.png"))).load());
+		imageMap.put("pine_tree", new Texture().image(loadImage(getFile("/images/pine_tree.png"))).load());
 		imageMap.put("chest", new Texture().image(loadImage(getFile("/images/chest.png"))).load());
+
 		imageMap.put("electrostatic_zapper",
 				new Texture().image(loadImage(getFile("/images/electrostatic_zapper.png"))).load());
 		imageMap.put("card_front", new Texture().image(loadImage(getFile("/images/card_front.png"))).load());
