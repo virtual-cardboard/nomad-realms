3f9c183854e3e4b19cc2d9fc95cdf100f7029fee
Lunkle <bobbob.donny@gmail.com>
2026-01-09 21:01:58 -0500

    TargetQuery and Conditions

---
+++ src/main/java/nomadrealms/context/game/card/condition/NotCondition.java
@@ -0,0 +1,19 @@
+package nomadrealms.context.game.card.condition;
+
+import nomadrealms.context.game.actor.cardplayer.CardPlayer;
+import nomadrealms.context.game.event.Target;
+import nomadrealms.context.game.world.World;
+
+public class NotCondition implements Condition {
+
+	private final Condition condition;
+
+	public NotCondition(Condition condition) {
+		this.condition = condition;
+	}
+
+	public boolean test(World world, Target target, CardPlayer source) {
+		return !condition.test(world, target, source);
+	}
+
+}
